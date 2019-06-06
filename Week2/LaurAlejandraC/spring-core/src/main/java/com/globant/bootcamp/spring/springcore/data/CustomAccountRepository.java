ae378dd4a3544739ce51e64dd99c2c49982a509e
Laura Chaparro Gutiérrez <lauralejandracg@gmail.com>
2019-06-06 09:50:14 -0500

    Change Resource annotation for Autowired in CustomAccountRepository

--- Week2/LaurAlejandraC/spring-core/src/main/java/com/globant/bootcamp/spring/springcore/data/CustomAccountRepository.java
+++ Week2/LaurAlejandraC/spring-core/src/main/java/com/globant/bootcamp/spring/springcore/data/CustomAccountRepository.java
@@ -1,15 +1,18 @@
 package com.globant.bootcamp.spring.springcore.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.globant.bootcamp.spring.springcore.domain.Account;
 
-import javax.annotation.Resource;
-
 @Repository
 public class CustomAccountRepository implements AccountRepository {
-    @Resource
-    DataSource dataSource;
+    private DataSource dataSource;
+
+    @Autowired
+    public CustomAccountRepository(final DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     public Account getAccount(String username) {
         return dataSource.retrieveAccountInformation(username);
